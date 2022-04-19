bf4ade5ee2a2bd13dc0e6810771c56a96ca40f4d
rolandexplore93 <roland2rule@gmail.com>
2022-04-19 08:35:38 +0100

    Explain Link, NavLink and styling of links in react

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import {Routes, Route} from 'react-router-dom'
 import { About } from './Components/About';
 import { Home } from './Components/Home';
+import { Navbar } from './Components/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='about' element={<About />} />
---
+++ src/Components/Navbar.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+export const Navbar = () => {
+
+    // NavLink component will set a property called isActive (true or false) which can be destructured from the function parameter
+    const StyleActiveNavbar = ({isActive}) => {
+        return {
+            // This return an object containing the styles
+            fontWeight: isActive ? "bold" : "normal",
+            textDecoration: isActive ? "none" : "underline"
+        }
+    }
+  return (
+    <nav>
+        {/* Link component renders an anchor element */}
+        {/* Add style prop to each component and set it to an arrow function */}
+        <NavLink style={StyleActiveNavbar} to='/'>Home</NavLink>
+        <NavLink style={StyleActiveNavbar} to='/about'>About</NavLink>
+    </nav>
+  )
+}
+
+// CSS into JS solution can also be used to style the active link
